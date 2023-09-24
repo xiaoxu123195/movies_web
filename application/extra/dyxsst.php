f6beec2e053aed8ef503a834f8ae5286f975ae17
xiaoxu123195 <2057099774@qq.com>
2023-09-24 10:32:06 +0800

    one

---
+++ application/extra/dyxsst.php
@@ -0,0 +1,55 @@
+<?php
+return array (
+  'dycms' => 
+  array (
+    's1' => 
+    array (
+      'logo1' => 'upload/dycms/20220504-1/afd38fa21fbac7b5e85e6b8b424a6fba.png',
+      'logo2' => 'upload/dycms/20220502-1/6b64b5b20a10607dad0c56cbc817eb8c.png',
+      'pic' => 'template/DYXS2/static/picture/loading.png',
+      'gg' => '莱恩影视',
+      'sm' => '本站资源来源于网络，只为学习交流使用！',
+    ),
+    's2' => 
+    array (
+      'wz' => '0',
+      'user' => '1',
+      'app' => '0',
+      'topic' => '0',
+      'diy1' => '0',
+      'diy1name' => '123',
+      'diy1url' => 'http://5.com/',
+      'diy2' => '0',
+      'diy2name' => '456',
+      'diy2url' => 'http://6.com/',
+      'slide' => '0',
+      'about' => '0',
+      'web1' => 'www.baidu.com',
+      'web2' => 'www.baidu2.com',
+      'web3' => '',
+      'tc' => '0',
+      'tc_noti' => '<p>本站为演示站</p>
+<p>欢迎体验</p>
+<p>完美2.0版</p>
+<p>不定时关闭</p>',
+      'app_url' => 'https://www.lanzoux.com/',
+      'app_url2' => '',
+      'miniplay' => '0',
+    ),
+    's3' => 
+    array (
+      'ad1' => '0',
+      'ad1_pc' => 'https://img14.360buyimg.com/ddimg/jfs/t1/174487/40/11344/6390/60ab7b9bEc2e0724c/e5688f83477adfc7.jpg',
+      'ad1_wap' => 'https://img10.360buyimg.com/ddimg/jfs/t1/195127/4/4693/12457/60ab7b9bE6f29557a/25ef8fce6fa2966f.jpg',
+      'ad1_url' => 'https://www.baidu.com',
+      'ad2' => '0',
+      'ad2_pc' => 'https://img14.360buyimg.com/ddimg/jfs/t1/174487/40/11344/6390/60ab7b9bEc2e0724c/e5688f83477adfc7.jpg',
+      'ad2_wap' => 'https://img10.360buyimg.com/ddimg/jfs/t1/195127/4/4693/12457/60ab7b9bE6f29557a/25ef8fce6fa2966f.jpg',
+      'ad2_url' => 'https://www.baidu.com',
+      'ad3' => '0',
+      'ad3_pc' => 'https://img14.360buyimg.com/ddimg/jfs/t1/174487/40/11344/6390/60ab7b9bEc2e0724c/e5688f83477adfc7.jpg',
+      'ad3_wap' => 'https://img10.360buyimg.com/ddimg/jfs/t1/195127/4/4693/12457/60ab7b9bE6f29557a/25ef8fce6fa2966f.jpg',
+      'ad3_url' => 'https://www.baidu.com',
+    ),
+  ),
+);
---
+++ application/extra/timming.php
@@ -0,0 +1,55 @@
+<?php
+return array (
+  'aa' => 
+  array (
+    'id' => 'aa',
+    '__token__' => 'fe8550d97f1c838e5373527e89679bd5',
+    'status' => '1',
+    'name' => 'aa',
+    'des' => '采集今日数据',
+    'file' => 'collect',
+    'param' => 'ac=cj&cjflag=b24c126793d9dbec81c9ca3a99bf41ba&cjurl=https%3A%2F%2Fapi.wujinapi.net%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Fwjm3u8%2F&h=24&t=&ids=&wd=&type=2&mid=1&opt=0&sync_pic_opt=0&filter=0&filter_from=&param=',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'runtime' => 1659150901,
+  ),
+  'bb' => 
+  array (
+    'id' => 'bb',
+    '__token__' => '548d8d372ca6d3fbb8e35edffb06ac9d',
+    'status' => '1',
+    'name' => 'bb',
+    'des' => '全网每日采集',
+    'file' => 'collect',
+    'param' => 'ac=cj&cjflag=3a4e1c6f613d66b5836bcbf23ce4c695&cjurl=http%3A%2F%2Fwww.zycaiji.net%3A7788%2Fapi.php%2Fprovide%2Fvod%2F%3Fac%3Dlist&h=24&t=&ids=&wd=&type=2&mid=1&opt=0&sync_pic_opt=0&filter=0&filter_from=&param=',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'runtime' => 1659152101,
+  ),
+  'cc' => 
+  array (
+    'id' => 'cc',
+    '__token__' => '6c577d898a0f7054e546e8abfadc1683',
+    'status' => '1',
+    'name' => 'cc',
+    'des' => '红牛',
+    'file' => 'collect',
+    'param' => 'ac=cj&cjflag=0388ded045382c5bf15ff53ec75d42c2&cjurl=https%3A%2F%2Fwww.hongniuzy2.com%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Fhnm3u8&h=24&t=&ids=&wd=&type=2&mid=1&opt=0&sync_pic_opt=0&filter=0&filter_from=&param=',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'runtime' => 1659151501,
+  ),
+  'dd' => 
+  array (
+    'id' => 'dd',
+    '__token__' => '8e47e2af54b41b06961a3ae4e43ad658',
+    'status' => '1',
+    'name' => 'dd',
+    'des' => '快播',
+    'file' => 'collect',
+    'param' => 'ac=cj&cjflag=b218dbd089fe71c6536cd0a17634ee9a&cjurl=http%3A%2F%2Fwww.kuaibozy.com%2Fapi.php%2Fprovide%2Fvod%2Ffrom%2Fkbm3u8%2Fat%2Fxml%2F&h=24&t=&ids=&wd=&type=1&mid=1&opt=0&sync_pic_opt=0&filter=0&filter_from=&param=',
+    'weeks' => '1,2,3,4,5,6,0',
+    'hours' => '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
+    'runtime' => 1659150301,
+  ),
+);
